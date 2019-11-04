b02a4b0bf4c18ddd9eec6b1c5e2fb2de82fecda3
Pere <pere.solaclaver@gmail.com>
2019-11-04 14:58:48 +0000

    get metod to pull account id

--- server.js
+++ server.js
@@ -6,17 +6,34 @@ const server = express();
 
 server.use(express.json());
 
+// ENDPOINTS
+
+// GET METHOD
+
 server.get("/", (req, res) => {
   db("accounts")
     .then(data => {
       res.status(200).json(data);
     })
     .catch(error => {
-      res
-        .status(500)
-        .json({
-          message: `There has been an error getting accounts: ${error.message}`
-        });
+      res.status(500).json({
+        message: `There has been an error getting accounts: ${error.message}`
+      });
+    });
+});
+
+// GET ACCOUNT METHOD
+
+server.get("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: `There has been an error getting account with id ${req.params.id}: ${error.message}`
+      });
     });
 });
 
