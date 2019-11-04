94014cb6f7d930713325e4157e4c274016192a7f
Pere <pere.solaclaver@gmail.com>
2019-11-04 14:55:05 +0000

    Get method pulling data from database

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.status(200).json('how r ya?')
-})
+server.get("/", (req, res) => {
+  db("accounts")
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          message: `There has been an error getting accounts: ${error.message}`
+        });
+    });
+});
 
 module.exports = server;
