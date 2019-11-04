2c6bb26ecac961e2c1060474f39400a2113a13ae
Pere <pere.solaclaver@gmail.com>
2019-11-04 15:13:18 +0000

    DELETE method

--- server.js
+++ server.js
@@ -43,10 +43,28 @@ server.post("/", (req, res) => {
   db("accounts")
     .insert(req.body)
     .then(data => {
-      res.status(200).json({...req.body, id: data[0]});
+      res.status(200).json({ ...req.body, id: data[0] });
     })
     .catch(error => {
       console.log(error.message);
+    });
+});
+
+// DELETE METHOD
+
+server.delete("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(data => {
+      res
+        .status(200)
+        .json({ message: `account ${req.params.id} deleted succesfully` });
+    })
+    .catch(data => {
+      res
+        .status(500)
+        .json({ message: `could not delet account ${id}: ${error.message}` });
     });
 });
 
